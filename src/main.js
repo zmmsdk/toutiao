bc905ac707f2ffa152b5923125db6e77136f3f1d
zmmsdk <you@example.com>
2019-12-18 16:11:45 +0800

    样式引入完成

--- src/main.js
+++ src/main.js
@@ -4,7 +4,10 @@ import router from './router'
 // 引入elementui并注册
 import ElementUI from 'element-ui' // 引入UI
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
-Vue.use(ElementUI)// 注册elementui
+// 注册elementui
+
+import './styles/index.less' // 引入样式
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
