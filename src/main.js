3dfd56113ddb5242b96e1097cadfceac31708e3b
zmmsdk <you@example.com>
2019-12-18 16:01:52 +0800

    elementui引入注册完成

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 引入elementui并注册
+import ElementUI from 'element-ui' // 引入UI
+import 'element-ui/lib/theme-chalk/index.css' // 引入样式
+Vue.use(ElementUI)// 注册elementui
 
 Vue.config.productionTip = false
 
